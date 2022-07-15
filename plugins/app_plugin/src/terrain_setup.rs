15183bcfe3eacdb5d7a00896840940a47e485a11
Kurt Kühnert <kurt@kuehnert.dev>
2022-07-15 11:36:27 +0200

    added experimental terrain material as bind group

--- plugins/app_plugin/src/terrain_setup.rs
+++ plugins/app_plugin/src/terrain_setup.rs
@@ -171,3 +171,52 @@ pub(crate) fn setup_density_texture(
         },
     );
 }
+
+pub(crate) fn setup_attachment(
+    config: &mut TerrainConfig,
+    from_disk_loader: &mut TextureAttachmentFromDiskLoader,
+    attachment_index: AttachmentIndex,
+    texture_size: u32,
+) {
+    let atlas_texture_descriptor = TextureDescriptor {
+        label: "".into(),
+        size: Extent3d {
+            width: texture_size,
+            height: texture_size,
+            depth_or_array_layers: config.node_atlas_size as u32,
+        },
+        mip_level_count: 1,
+        sample_count: 1,
+        dimension: TextureDimension::D2,
+        format: TextureFormat::R16Unorm,
+        usage: TextureUsages::COPY_DST | TextureUsages::TEXTURE_BINDING,
+    };
+
+    let view_descriptor = TextureViewDescriptor {
+        label: "height_texture_attachment_view".into(),
+        dimension: Some(TextureViewDimension::D2Array),
+        ..default()
+    };
+
+    let mut node_texture_descriptor = atlas_texture_descriptor.clone();
+    node_texture_descriptor.label = "node_height_texture_attachment".into();
+    node_texture_descriptor.size.depth_or_array_layers = 1;
+    node_texture_descriptor.usage |= TextureUsages::COPY_SRC;
+
+    from_disk_loader.add_attachment(
+        attachment_index,
+        TextureAttachmentFromDisk {
+            path: config.path.clone() + "data/height",
+            texture_descriptor: node_texture_descriptor,
+        },
+    );
+
+    config.add_attachment(
+        attachment_index,
+        AtlasAttachmentConfig::Texture {
+            texture_size,
+            texture_descriptor: atlas_texture_descriptor,
+            view_descriptor,
+        },
+    );
+}
